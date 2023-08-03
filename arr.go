f350d11dd139655f45dc40bb7eee4ba32130b70d
Yiying Chen <bysnn@qq.com>
2023-08-03 19:58:31 +0800

    Add arr.go for Array operations

---
+++ arr.go
@@ -0,0 +1,14 @@
+package gotest
+
+import(
+"errors"
+)
+
+func ArrFirst(arr []int) (int, error) {
+    if len(arr)>0 {
+        return arr[0], nil
+    } else {
+        return 0, errors.New("arr is empty")
+    }
+
+}
