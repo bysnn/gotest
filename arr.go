96759a1192f9ed846e7e18e0742913380ba0cca9
Yiying Chen <bysnn@qq.com>
2023-08-03 21:00:57 +0800

    Add ArrEnd()

--- arr.go
+++ arr.go
@@ -4,10 +4,21 @@ import(
 "errors"
 )
 
+
 func ArrFirst(arr []int) (int, error) {
     if len(arr)>0 {
         return arr[0], nil
     } else {
         return 0, errors.New("arr is empty")
     }
+}
+
+func ArrEnd(arr []int) (int, error) {
+    count=len(arr)
+    if count>0 {
+        return arr[count-1], nil
+    } else {
+        return 0, errors.New("arr is empty")
+    }
+    
 }
